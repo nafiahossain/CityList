73a50c2808ff3dc4a41c08daaddfd44eff9285b9
nafiahossain <nafiahossain12@gmail.com>
2022-06-04 22:29:20 +0600

    Committed

--- app/src/main/java/com/example/citylist/CityList.java
+++ app/src/main/java/com/example/citylist/CityList.java
@@ -2,6 +2,7 @@ package com.example.citylist;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -27,9 +28,52 @@ public class CityList {
      * @return
      *      Return the sorted list of cities
      */
-    public List<City> getCities() {
+    public List<City> getCities(int param) {
         List<City> cityList = cities;
-        Collections.sort(cityList);
+        if (param==1)
+        {
+            Collections.sort(cityList);
+        }
+        else
+        {
+            Collections.sort(cityList, new Comparator<City>() {
+                @Override
+                public int compare(City city, City o2) {
+                    return city.getProvinceName().compareTo(o2.getProvinceName());
+                }
+            });
+        }
+
         return cityList;
+    }
+
+    /**
+     * This is a javadoc for the method "delete"
+     * This method removes an element from the list
+     * If the desired element to be deleted is not on
+     * the list, it will throw an exception
+     * @param city
+     * This is a void method with a parameter
+     */
+    public void delete(City city)
+    {
+        if (!cities.contains(city))
+        {
+            throw new IllegalArgumentException();
+        }
+        else
+        {
+            cities.remove(city);
+        }
+    }
+
+    /**
+     * This is a javadoc for the method "count"
+     * @return this method returns the size of "cities" arraylist
+     * This is not a void method
+     */
+    public int count()
+    {
+        return cities.size();
     }
 }
